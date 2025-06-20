000154869241a12ff151a6ee08584ae6ac5ea153
Ege Oguz <iegeoguz@gmail.com>
2025-06-20 23:20:44 +0200

    Add function prototypes for Matrix class

---
+++ Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by İsmail Ege Oğuz on 20.06.2025.
+//
+
+#include "Matrix.h"
---
+++ Matrix.h
@@ -0,0 +1,26 @@
+//
+// Created by İsmail Ege Oğuz on 20.06.2025.
+//
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <vector>
+
+class Matrix {
+private:
+    int rows, columns;
+    std::vector<std::vector<double>> data;
+public:
+    Matrix(int rows, int columns);
+    void fillRandom();
+    void print();
+    Matrix add(const Matrix& B);
+    Matrix multiply(const Matrix& B);
+    Matrix transpose();
+    Matrix map();
+};
+
+
+
+#endif //MATRIX_H
