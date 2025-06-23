0a249262a34e0fa8fc724b4f77552cbc70e9d0bf
Ege Oğuz <iegeoguz@gmail.com>
2025-06-23 12:00:00 +0200

    Test fill random function in the  Matrix class

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
     m.setValueAt(0,0, 7);
     m.setValueAt(2,0, 7);
     Matrix c = m.add(m2);
+    c.fillRandom();
     c.print();
     std::cout << std::endl;
     Matrix d = c.transpose();
